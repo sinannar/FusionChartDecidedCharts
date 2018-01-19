a3d78a9dfb6cda5002d54bb851617bee25251bd9
Sinan NAR <sinan.nar@gmail.com>
2018-01-19 14:00:19 +1300

    colum2d has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   data: Object;
-  pie2d_data:Object;
-  doughnut2d_data:Object;
   pie_data:Object;
   doughnut_data:Object;
-
+  column_data:Object
   
   constructor() {
     this.data = {
@@ -94,6 +92,52 @@ export class AppComponent {
           "value": "49100"
       }]
     };
+
+    //https://www.fusioncharts.com/dev/chart-guide/standard-charts/configuring-standard-charts.html
+    this.column_data = {
+      "chart": {
+          "caption": "Quarterly Sales Summary",
+          "subcaption": "Last year",
+          "xaxisname": "Quarter",
+          "yaxisname": "Amount (In USD)",
+          "numberprefix": "$",
+          "theme": "fint"
+      },
+      "data": [
+          {
+              "label": "Q1",
+              "value": "195000"
+          },
+          {
+              "label": "Q2",
+              "value": "155000"
+          },
+          {
+              "label": "Q3",
+              "value": "178000"
+          },
+          {
+              "label": "Q4",
+              "value": "192000"
+          }
+      ],
+      "trendlines": [
+          {
+              "line": [
+                  {
+                      "isTrendZone": "1",
+                      "startvalue": "175000",
+                      "endValue": "150000",
+                      "color": "#1aaf5d",
+                      "valueOnRight": "1",
+                      "alpha": "30",
+                      "tooltext": "Quarterly sales target range was $endDataValue to $startDataValue",
+                      "displayvalue": "Target Range"
+                  }
+              ]
+          }
+      ]
+  }
 
 
 
